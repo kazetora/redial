149a303a91ea94ea91ff86908f919f906f3a01c2
putra <putra@nakao-lab.org>
2015-06-12 02:33:30 +0000

    Add socket client for notification

--- client/clientWatcher.py
+++ client/clientWatcher.py
@@ -6,7 +6,10 @@ import fcntl
 import struct
 import array
 import xmlrpclib
+import urllib
+import urllib2
 import time
+from socketIO_client import SocketIO, LoggingNamespace
 
 class ClientWatcher:
     def __init__(self, nodeId, serveraddr, serverport):
@@ -15,15 +18,20 @@ class ClientWatcher:
         self.server_port = serverport
         #self.updated = False
         self.dialing = False
+        self.connected = False
+        self.API_SERVER = "http://%s:%d" % (self.server_addr, self.server_port)
+        self.WS_SERVER = "ws://%s:%d/socket.io/1/websocket" % (self.server_addr, self.server_port)
         found = False
         for i in self.all_interfaces():
             if i == "ppp0":
                 found = True
                 break
         if found:
-            proxy = xmlrpclib.ServerProxy("http://%s:%d/" % (self.server_addr, self.server_port))
-            ret = proxy.updateClientList(self.nodeId, self.get_ip_address("ppp0"))
-            print "update client: %s" % ret
+            #proxy = xmlrpclib.ServerProxy("http://%s:%d/" % (self.server_addr, self.server_port))
+            #ret = proxy.updateClientList(self.nodeId, self.get_ip_address("ppp0"))
+            print "update client" 
+            self.updateNodeIP(self.get_ip_address("ppp0"))
+            
 
     def get_ip_address(self, ifname):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -57,20 +65,42 @@ class ClientWatcher:
                 found = True
                 break
         if not found:
-            if not self.dialing:
-                print "ppp0 is down. redialing now"
-                self.dialing = True
-                os.system("wvdial > /dev/null &")
+            print "ppp0 is down. redialing now"
+            self.dialing = True
+            os.system("wvdial > /dev/null &")
+            time.sleep(5)
         else:
             if self.dialing:
                 self.dialing = False
                 print "updating client info"
-                proxy = xmlrpclib.ServerProxy("http://%s:%d/" % (self.server_addr, self.server_port))
-                ret = proxy.updateClientList(self.nodeId, self.get_ip_address("ppp0"))
-                print "update client: %s" % ret
-    
+                #proxy = xmlrpclib.ServerProxy("http://%s:%d/" % (self.server_addr, self.server_port))
+                #ret = proxy.updateClientList(self.nodeId, self.get_ip_address("ppp0"))
+                #print "update client: %s" % ret
+                self.updateNodeIP(self.get_ip_address("ppp0"))
+
+    def updateNodeIP(self, ip):
+        url = "%s/nodes/updateNodeIP/" % self.API_SERVER
+        print url
+        params = urllib.urlencode({
+             'id': self.nodeId,
+             'ip': ip
+        })
+        print params
+        req = urllib2.Request(url)
+        req.add_data(params)
+        response = urllib2.urlopen(req).read()
+        print "response: %s" % response
+        self.sendUpdateCompleteMessage()
+
+
+    def sendUpdateCompleteMessage(self):
+        with SocketIO(self.server_addr, self.server_port, LoggingNamespace) as socketIO:
+            socketIO.emit('update_complete')
+            socketIO.wait(seconds=1)
+        
+
 if __name__ == "__main__":
-    cw = ClientWatcher(1, "133.11.240.226", 8888)
+    cw = ClientWatcher("4JbCfm9S", "192.168.110.131", 3000)
     while True:
         cw.updateClientInfo()
         time.sleep(1)
